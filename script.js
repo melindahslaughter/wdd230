e07c832d2b6c84b652639ba054cc36132eb91301
melindahslaughter <mslaughter1@byui.edu>
2022-09-16 12:08:21 -0600

    new file:   images/hphoto.jpg 	modified:   index.html 	new file:   script.js 	new file:   styles.css

---
+++ script.js
@@ -0,0 +1,17 @@
+// const options = {
+//   weekday: "short",
+//   day: "numeric",
+//   month: "long",
+//   year: "numeric",
+// };
+
+const nyear = new Date();
+document.getElementById(
+  "copyyear"
+).textContent = ` ${nyear.getFullYear()} | Melinda Slaughter | Sheridan, Wyoming`;
+
+
+const time = new Date(document.lastModified);
+
+document.getElementById("copyright").textContent = ` Last updated: ${time.getDate()}/${time.getMonth()}/${time.getFullYear()}
+ ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
