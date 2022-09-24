981f4314295e7f4538eca15e1d580ae9d3ad3b6b
melindahslaughter <mslaughter1@byui.edu>
2022-09-23 20:28:15 -0600

    9/23/22 deposit

---
+++ design-principles.html/script.js
@@ -0,0 +1,11 @@
+const nyear = new Date();
+document.getElementById(
+  "copyyear"
+).textContent = ` ${nyear.getFullYear()} .:|:. Melinda Slaughter .:|:. Sheridan, Wyoming`;
+
+const time = new Date(document.lastModified);
+
+document.getElementById(
+  "copyright"
+).textContent = ` Last updated: ${time.getDate()}/${time.getMonth()}/${time.getFullYear()}
+ ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
