c5ebb6c2af87488d78f80d377795a2126deb00ad
melindahslaughter <mslaughter1@byui.edu>
2022-11-03 16:34:15 -0600

    join page push

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,7 +1,7 @@
- document.querySelector(
-   "#lastModified"
+document.querySelector(
+	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
- 
+
 // select the elements to manipulate (output to)
 const datefield = document.querySelector(".date");
 const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
@@ -18,27 +18,39 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
+document.querySelector(".dateTime").value = fulldate; //add date to hidden page.
+
 
 function toggleMenu() {
 	document.getElementById('primaryNav').classList.toggle("open");
 	document.getElementById("hamburgerBtn").classList.toggle("open");
-	
+
 }
 
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
-	let dayOfWeek = new Date();
-	if (dayOfWeek.getDay() == 2 || dayOfWeek.getDay() == 3) {
+let dayOfWeek = new Date();
+if (dayOfWeek.getDay() == 2 || dayOfWeek.getDay() == 3) {
 
-		document.querySelector('.text-block').style.display = "block"
-	
-	}
-	
+	document.querySelector('.text-block').style.display = "block"
 
-	
+}
 
-                   
+// const input = document.querySelector(".title")
+// const regEx = /^[a-zA-z- ] {7}$/;
+// if (input.ariaValueMax.match(regEx)) {
+// 	return true;
+// }
+// else {
+// 	alert("Please enter a valid input.");
+// 	return false;
+// }
+
+
+
+
+
 
 
 
