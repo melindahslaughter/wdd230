d2a8260b24fc941fddd70ee22231cdb39c583a7e
melindahslaughter <mslaughter1@byui.edu>
2022-11-15 12:48:56 -0700

    CSS added

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,9 +1,10 @@
 const currentTemp = document.querySelector('#current-temp');
+const currentWind = document.querySelector("#windspeed")
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const url = "https://api.openweathermap.org/data/2.5/weather?zip=99709&appid=364ee35cfabcb4fec9d4af380e8686be&units=imperial";
 // const url = "https://api.openweathermap.org/geo/1.0/direct?q=Fairbanks&appid=5d531f5cda0a10a27488af4ab3a16859&units=imperial";
-//use this api key  c   in a few hours. 
+
 
 async function apiFetch() {
     try {
@@ -11,7 +12,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data); // this is for testing the call
-            // displayResults(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -23,7 +24,7 @@ async function apiFetch() {
 //apiFetch();
 function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-
+    currentWind.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
 
