b2cada0ebf63421ac996458fa5e7b249a9026a3b
melindahslaughter <mslaughter1@byui.edu>
2022-11-29 15:16:13 -0700

    css changes

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -31,6 +31,15 @@ const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
 
+// let todayDate = Date.now();
+// let lastDate = (window.localStorage.getItem("lastVisit"));
+
+// let numDays = todayDate - lastDate;
+
+// let day = (numDays / (1000 * 60 * 60 * 24));
+// document.querySelector(".different").textContent = day.toFixed(0);
+
+// window.localStorage.setItem("lastVisit", todayDate);
 
 
 
