093d79abf0ab27f47f92d9feaffdb130fbcca2b2
melindahslaughter <mslaughter1@byui.edu>
2022-11-30 07:11:49 -0700

    commit for wave

--- chamber/scripts/chamweather.js
+++ chamber/scripts/chamweather.js
@@ -22,9 +22,9 @@ async function apiFetch() {
 function setWindChill(t, s) {
     if (t <= 50 && s >= 3) {
         let windchill =
-            35.74 +
-            0.6215 * t -
-            35.75 * Math.pow(s, 0.16) +
+            (35.74 +
+            (0.6215 * t)) -
+            (35.75 * Math.pow(s, 0.16)) +
             0.4275 * t * Math.pow(s, 0.16);
         return Math.round(windchill);
     }
--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -36,11 +36,13 @@ let lastDate = (window.localStorage.getItem("lastVisit"));
 
 let numDays = todayDate - lastDate;
 
-let day = (numDays / (1000 * 60 * 60 * 24));
+if (lastDate !== null) {
+	let day = (numDays / (1000 * 60 * 60 * 24));
+
 document.querySelector(".different").textContent = day.toFixed(0);
 
 window.localStorage.setItem("lastVisit", todayDate);
-
+}
 
 
 
